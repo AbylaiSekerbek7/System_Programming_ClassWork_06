82b2e8c79151e96c3767939c97e8448db5ec9f96
Секербек Абылайхан Кайратулы <SekerbekA@lcoast.itstep>
2024-03-28 21:52:43 +0500

    New changes

---
+++ SampleSemaphore/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace SampleSemaphore
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.lbWait = new System.Windows.Forms.ListBox();
+      this.lbActive = new System.Windows.Forms.ListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.txtLog = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.txtCntAll = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.txtCntActive = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.btnCreate = new System.Windows.Forms.Button();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.btnClose = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // lbWait
+      // 
+      this.lbWait.FormattingEnabled = true;
+      this.lbWait.HorizontalScrollbar = true;
+      this.lbWait.Location = new System.Drawing.Point(12, 38);
+      this.lbWait.Name = "lbWait";
+      this.lbWait.Size = new System.Drawing.Size(184, 212);
+      this.lbWait.TabIndex = 0;
+      // 
+      // lbActive
+      // 
+      this.lbActive.FormattingEnabled = true;
+      this.lbActive.HorizontalScrollbar = true;
+      this.lbActive.Location = new System.Drawing.Point(202, 38);
+      this.lbActive.Name = "lbActive";
+      this.lbActive.Size = new System.Drawing.Size(184, 212);
+      this.lbActive.TabIndex = 1;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 20);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(108, 13);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Ожидающие потоки";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(199, 20);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(95, 13);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Активные потоки";
+      // 
+      // txtLog
+      // 
+      this.txtLog.Location = new System.Drawing.Point(12, 283);
+      this.txtLog.Multiline = true;
+      this.txtLog.Name = "txtLog";
+      this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.txtLog.Size = new System.Drawing.Size(374, 188);
+      this.txtLog.TabIndex = 4;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 267);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(83, 13);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Вывод, журнал";
+      // 
+      // txtCntAll
+      // 
+      this.txtCntAll.Location = new System.Drawing.Point(408, 38);
+      this.txtCntAll.Name = "txtCntAll";
+      this.txtCntAll.Size = new System.Drawing.Size(100, 20);
+      this.txtCntAll.TabIndex = 6;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(405, 20);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(111, 13);
+      this.label4.TabIndex = 7;
+      this.label4.Text = "Кол-во всех потоков";
+      // 
+      // txtCntActive
+      // 
+      this.txtCntActive.Location = new System.Drawing.Point(408, 84);
+      this.txtCntActive.Name = "txtCntActive";
+      this.txtCntActive.Size = new System.Drawing.Size(100, 20);
+      this.txtCntActive.TabIndex = 8;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(405, 68);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(136, 13);
+      this.label5.TabIndex = 9;
+      this.label5.Text = "Кол-во активных потоков";
+      // 
+      // btnCreate
+      // 
+      this.btnCreate.Location = new System.Drawing.Point(408, 143);
+      this.btnCreate.Name = "btnCreate";
+      this.btnCreate.Size = new System.Drawing.Size(144, 23);
+      this.btnCreate.TabIndex = 10;
+      this.btnCreate.Text = "Создать потоки";
+      this.btnCreate.UseVisualStyleBackColor = true;
+      this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(408, 172);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(144, 23);
+      this.btnStart.TabIndex = 11;
+      this.btnStart.Text = "Запустить потоки";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // btnClose
+      // 
+      this.btnClose.Location = new System.Drawing.Point(441, 227);
+      this.btnClose.Name = "btnClose";
+      this.btnClose.Size = new System.Drawing.Size(75, 23);
+      this.btnClose.TabIndex = 12;
+      this.btnClose.Text = "Закрыть";
+      this.btnClose.UseVisualStyleBackColor = true;
+      this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(615, 483);
+      this.Controls.Add(this.btnClose);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.btnCreate);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.txtCntActive);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.txtCntAll);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.txtLog);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.lbActive);
+      this.Controls.Add(this.lbWait);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox lbWait;
+    private System.Windows.Forms.ListBox lbActive;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox txtLog;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox txtCntAll;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox txtCntActive;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Button btnCreate;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Button btnClose;
+  }
+}
+
---
+++ SampleSemaphore/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SampleSemaphore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // список потоков в ожидании
+        public List<Thread> lstThWait = new List<Thread>();
+
+        // список активных потоков
+        public List<Thread> lstThActive = new List<Thread>();
+
+        // событие для синхронного старта потоков
+        public ManualResetEvent evnStart = new ManualResetEvent(false);
+
+        // семафор для потоков
+        public Semaphore semaphore = null;
+
+        // объект мьютекс для сихронизации вывода в поле txtLog
+        public Mutex mtxLog = new Mutex();
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            int cntAll = 0;
+            int cntActive = 0;
+            if (!(int.TryParse(txtCntAll.Text, out cntAll) && int.TryParse(txtCntActive.Text, out cntActive)))
+            {
+                txtLog.Text += "Error: incorrect input for all thread cnt or active thread!\r\n";
+                txtCntAll.Focus();
+                txtCntAll.SelectAll();
+
+                txtCntActive.Focus();
+                txtCntActive.SelectAll();
+                return;
+            }
+            
+            semaphore = new Semaphore(cntActive, cntActive);
+            evnStart.Reset();
+            for (int i = 0; i < cntAll; ++i)
+            {
+                Thread th = new Thread(ThreadProc);
+                th.IsBackground = true;
+                th.Name = $"Thread №{i + 1}";
+                th.Start(this);
+                lstThWait.Add(th);
+            }
+            ListUpdate();
+        } // private void btnCreate_Click();
+
+        public void ListUpdate()
+        {
+            if (lbWait.InvokeRequired)
+            {
+                lbWait.Invoke(new Action(() =>
+                {
+                    lbWait.Items.Clear();
+                    foreach (Thread t in lstThWait)
+                    {
+                        string str = t.ManagedThreadId.ToString() + ": " + t.Name;
+                        lbWait.Items.Add(str);
+                    }
+                    lbActive.Items.Clear();
+                    foreach (Thread t in lstThActive)
+                    {
+                        string str = t.ManagedThreadId.ToString() + ": " + t.Name;
+                        lbActive.Items.Add(str);
+                    }
+                }));
+            }
+            else
+            {
+                lbWait.Items.Clear();
+                foreach (Thread t in lstThWait)
+                {
+                    string str = t.ManagedThreadId.ToString() + ": " + t.Name;
+                    lbWait.Items.Add(str);
+                }
+                lbActive.Items.Clear();
+                foreach (Thread t in lstThActive)
+                {
+                    string str = t.ManagedThreadId.ToString() + ": " + t.Name;
+                    lbActive.Items.Add(str);
+                }
+            }
+        } // public void ListUpdate();
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            evnStart.Set();
+        } // private void btnStart_Click();
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void ThreadProc(object Param)
+        {
+            Form1 form1 = Param as Form1;
+
+            form1.evnStart.WaitOne();
+            form1.semaphore.WaitOne();
+
+            Thread t = Thread.CurrentThread;
+            form1.mtxLog.WaitOne();
+            {
+                form1.lstThWait.Remove(t);
+                form1.lstThActive.Add(t);
+            }
+            form1.mtxLog.ReleaseMutex();
+            form1.ListUpdate();
+
+            form1.Invoke(new Action(() => {
+                form1.txtLog.Text += $"Thread ID {t.ManagedThreadId} started\r\n";
+            }));
+
+            // Иммитация работы потока
+
+            Random rnd = new Random(t.ManagedThreadId);
+            int sleep = rnd.Next(1000, 4000);
+            Thread.Sleep(sleep);
+
+            form1.mtxLog.WaitOne();
+            {
+                form1.lstThActive.Remove(t);
+            }
+            form1.mtxLog.ReleaseMutex();
+            form1.ListUpdate();
+
+            form1.Invoke(new Action(() =>
+            {
+                form1.txtLog.Text += $"Thread ID {t.ManagedThreadId} exit\r\n";
+            }));
+
+            form1.semaphore.Release();
+        } // конец ф-ции потока
+    }
+}
